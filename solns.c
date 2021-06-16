469a5cd8585e879524d95a31c3eef4ca6151cb70
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-06-16 10:43:49 +0530

    Adds max and min functions

--- solns.c
+++ solns.c
@@ -1,2 +1,30 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+/*Max function*/
+int max(int array[], int num){
+         int max = array[0];
+         for(int i=0; i<num; i++){
+                if(max<array[i]){
+                        max = array[i];
+                }
+         
+         }
+        
+        return max;
+}
+
+
+/*min function*/
+
+int min(int array[], int num){
+         int min = array[0];
+         for(int i=0; i<num; i++){
+                if(min>array[i]){
+                        min = array[i];
+                }
+         
+         }
+        
+        return min;
+}
